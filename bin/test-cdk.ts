e16e434fbfcaac4be4357f2d27286982c52a58b7
BioCarmen <carmen@biorender.com>
2022-06-01 21:28:26 -0400

    all

--- bin/test-cdk.ts
+++ bin/test-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { TestCdkStack } from '../lib/test-cdk-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { TestCdkStack } from "../lib/test-cdk-stack";
 
 const app = new cdk.App();
-new TestCdkStack(app, 'TestCdkStack', {
+new TestCdkStack(app, "TestCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +18,10 @@ new TestCdkStack(app, 'TestCdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: "355621124855",
+    region: "us-east-1",
+  },
 });
+
+app.synth();
--- lib/test-cdk-stack.ts
+++ lib/test-cdk-stack.ts
@@ -1,5 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestCdkStack extends Stack {
@@ -12,5 +17,12 @@ export class TestCdkStack extends Stack {
     // const queue = new sqs.Queue(this, 'TestCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "MyPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("BioCarmen/test-cdk", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
