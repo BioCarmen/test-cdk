c5643ea896223b929ffa534a4947dcfb95d72410
BioCarmen <carmen@biorender.com>
2022-06-01 22:08:33 -0400

    add lambda function

--- lib/test-cdk-stack.ts
+++ lib/test-cdk-stack.ts
@@ -2,9 +2,11 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import {
   CodePipeline,
   CodePipelineSource,
+  ManualApprovalStep,
   ShellStep,
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { MyPipelineAppStage } from "./stage";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestCdkStack extends Stack {
@@ -24,5 +26,23 @@ export class TestCdkStack extends Stack {
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
+    const testingStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "test", {
+        env: { account: "355621124855", region: "us-east-1" },
+      })
+    );
+
+    testingStage.addPre(
+      new ShellStep("Run Unit Tests", { commands: ["npm install", "npm test"] })
+    );
+    testingStage.addPost(
+      new ManualApprovalStep("Manual approval before production")
+    );
+
+    const prodStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "prod", {
+        env: { account: "355621124855", region: "us-east-1" },
+      })
+    );
   }
 }
