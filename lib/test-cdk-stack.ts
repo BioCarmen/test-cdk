1c319b287c56c163d6df91aa332310c7fb599296
BioCarmen <carmen@biorender.com>
2022-06-03 14:29:37 -0400

    test

--- lib/test-cdk-stack.ts
+++ lib/test-cdk-stack.ts
@@ -46,24 +46,24 @@ export class TestCdkStack extends Stack {
         buildCommand: "npm run build",
       }),
     });
-    const secret = secretsmanager.Secret.fromSecretNameV2(
-      this,
-      "arn:aws:secretsmanager:us-east-1:355621124855:secret:github-token-b7BN8L",
-      "github-token"
-    ).toString();
-    console.log(secret);
-    const post = new CodePipelinePostToGitHub(
-      this,
-      "CodePipelinePostToGithub",
-      {
-        pipeline: pipeline.codePipeline,
-        githubToken: secretsmanager.Secret.fromSecretNameV2(
-          this,
-          "arn:aws:secretsmanager:us-east-1:355621124855:secret:github-token-b7BN8L",
-          "github-token"
-        ).toString(),
-      }
-    );
+    // const secret = secretsmanager.Secret.fromSecretNameV2(
+    //   this,
+    //   "arn:aws:secretsmanager:us-east-1:355621124855:secret:github-token-b7BN8L",
+    //   "github-token"
+    // ).toString();
+    // console.log(secret);
+    // const post = new CodePipelinePostToGitHub(
+    //   this,
+    //   "CodePipelinePostToGithub",
+    //   {
+    //     pipeline: pipeline.codePipeline,
+    //     githubToken: secretsmanager.Secret.fromSecretNameV2(
+    //       this,
+    //       "arn:aws:secretsmanager:us-east-1:355621124855:secret:github-token-b7BN8L",
+    //       "github-token"
+    //     ).toString(),
+    //   }
+    // );
 
     const preprod = new MyPipelineAppStage(this, "test", {
       env: { account: "355621124855", region: "us-east-1" },
