0e31e6a5e3e3a100e49d4ec29c238b24d4a10825
BioCarmen <carmen@biorender.com>
2022-06-03 20:49:29 -0400

    add things

--- lib/lambda/github-handler.ts
+++ lib/lambda/github-handler.ts
@@ -1,33 +1,76 @@
+import { CodePipeline } from "@aws-cdk/pipelines";
+import fetch from "node-fetch";
+import * as AWS from "aws-sdk";
 export const handler = async (event: any) => {
   console.log(event);
-  //   const region = event.region;
-  //   const pipelineName = event.detail.pipeline;
-  //   const executionId = event.detail["execution-id"];
-  //   const state = transformState(event.detail.state);
+  const region = event.region;
+  const pipelineName = event.detail.pipeline;
+  const executionId = event.detail["execution-id"];
+  const state = transformState(event.detail.state);
 
-  //   if (state === null) {
-  //     return;
-  //   }
+  if (state === null) {
+    return;
+  }
 
-  //   const result = await getPipelineExecution(pipelineName, executionId);
-  //   const payload = createPayload(pipelineName, region, state);
+  const result = await getPipelineExecution(pipelineName, executionId);
+  const payload = createPayload(pipelineName, region, state);
 
-  //   if (!result) {
-  //     console.error(`Can not resolve pipeline execution`);
-  //     return;
-  //   }
+  if (!result) {
+    console.error(`Can not resolve pipeline execution`);
+    return;
+  }
 
-  //   await postStatusToGitHub(
-  //     result.owner,
-  //     result.repository,
-  //     result.sha,
-  //     payload
-  //   );
+  await postStatusToGitHub(
+    result.owner,
+    result.repository,
+    result.sha,
+    payload
+  );
 
-  //   console.log(
-  //     `Successfully notified GitHub repository ${result.owner}/${result.repository} for commit ${result.sha} with payload:`,
-  //     payload
-  //   );
+  console.log(
+    `Successfully notified GitHub repository ${result.owner}/${result.repository} for commit ${result.sha} with payload:`,
+    payload
+  );
+};
+const getPersonalAccessToken = () => {
+  if (process.env.ACCESS_TOKEN) {
+    return process.env.ACCESS_TOKEN as string;
+  }
+  throw new Error("process.env.ACCESS_TOKEN is not defined");
+};
+
+const getPipelineExecution = async (
+  pipelineName: string,
+  executionId: string
+) => {
+  const params = {
+    pipelineName: pipelineName,
+    pipelineExecutionId: executionId,
+  };
+
+  const result = await new AWS.CodePipeline()
+    .getPipelineExecution(params)
+    .promise();
+  const artifactRevision = result?.pipelineExecution?.artifactRevisions?.find(
+    () => true
+  );
+
+  const revisionURL = artifactRevision?.revisionUrl;
+  const sha = artifactRevision?.revisionId;
+
+  if (!revisionURL || !sha) {
+    console.error("No revision URL or commit hash resolved");
+    return;
+  }
+
+  const pattern = /github.com\/(.+)\/(.+)\/commit\//;
+  const matches = pattern.exec(revisionURL);
+
+  return {
+    owner: matches?.[1],
+    repository: matches?.[2],
+    sha: sha,
+  };
 };
 
 function transformState(state: string) {
@@ -43,6 +86,41 @@ function transformState(state: string) {
 
   return null;
 }
+function createPayload(pipelineName: string, region: string, status: string) {
+  let description;
+  if (status === "pending") {
+    description = "Build started";
+  } else if (status === "success") {
+    description = "Build succeeded";
+  } else if (status === "failure") {
+    description = "Build failed!";
+  }
+
+  return {
+    state: status,
+    target_url: pipelineName,
+    description: description,
+    context: `ci/${pipelineName}/${region}`,
+  };
+}
+
+const postStatusToGitHub = async (
+  owner: string | undefined,
+  repository: string | undefined,
+  sha: any,
+  payload: any
+) => {
+  const url = `/${owner}/${repository}/statuses/${sha}`;
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: getPersonalAccessToken(),
+    },
+    body: payload,
+  });
+};
 
 // const getPipelineExecution = async (pipelineName: string, executionId: string) => {
 //     const params = {
