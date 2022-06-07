2244eb4d3678515eb1c714bd25879e7d8c8a56fc
BioCarmen <carmen@biorender.com>
2022-06-06 20:42:07 -0400

    test

--- lib/main-stack.ts
+++ lib/main-stack.ts
@@ -56,60 +56,62 @@ export class TestCdkStack extends Stack {
       region = "us-east-1",
       secretName =
         "arn:aws:secretsmanager:us-east-1:355621124855:secret:github-token-b7BN8L",
-      secret,
+      secret: any,
       decodedBinarySecret;
 
     // Create a Secrets Manager client
     var client = new AWS.SecretsManager({
       region: region,
     });
-    let githubToken = "";
+
     // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
     // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     // We rethrow the exception by default.
 
-    client.getSecretValue(
-      { SecretId: secretName },
-      function (err: any, data: any) {
-        if (err) {
-          if (err.code === "DecryptionFailureException")
-            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-          else if (err.code === "InternalServiceErrorException")
-            // An error occurred on the server side.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-          else if (err.code === "InvalidParameterException")
-            // You provided an invalid value for a parameter.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-          else if (err.code === "InvalidRequestException")
-            // You provided a parameter value that is not valid for the current state of the resource.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
-          else if (err.code === "ResourceNotFoundException")
-            // We can't find the resource that you asked for.
-            // Deal with the exception here, and/or rethrow at your discretion.
-            throw err;
+    client.getSecretValue({ SecretId: secretName }, (err: any, data: any) => {
+      if (err) {
+        if (err.code === "DecryptionFailureException")
+          // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "InternalServiceErrorException")
+          // An error occurred on the server side.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "InvalidParameterException")
+          // You provided an invalid value for a parameter.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "InvalidRequestException")
+          // You provided a parameter value that is not valid for the current state of the resource.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "ResourceNotFoundException")
+          // We can't find the resource that you asked for.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+      } else {
+        // Decrypts secret using the associated KMS key.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ("SecretString" in data) {
+          secret = data.SecretString;
         } else {
-          // Decrypts secret using the associated KMS key.
-          // Depending on whether the secret is a string or binary, one of these fields will be populated.
-          if ("SecretString" in data) {
-            githubToken = data.SecretString;
-          } else {
-            let buff = new Buffer(data.SecretBinary, "base64");
-            decodedBinarySecret = buff.toString("ascii");
-          }
+          let buff = new Buffer(data.SecretBinary, "base64");
+          decodedBinarySecret = buff.toString("ascii");
         }
-
-        // Your code goes here.
       }
-    );
-    const preprod = new MyPipelineAppStage(this, "test", {
-      env: { account: "355621124855", region: "us-east-1" },
+
+      // Your code goes here.
+      const preprod = new MyPipelineAppStage(this, "test", {
+        env: { account: "355621124855", region: "us-east-1" },
+      });
+      pipeline.addApplicationStage(preprod);
+
+      new CodePipelinePostToGitHub(this, "CodePipelinePostToGithub", {
+        pipeline: pipeline.codePipeline,
+        githubToken: secret,
+      });
     });
-    pipeline.addApplicationStage(preprod);
 
     // const stage = pipeline.addStage("Approval");
 
@@ -125,10 +127,6 @@ export class TestCdkStack extends Stack {
     //   })
     // );
 
-    new CodePipelinePostToGitHub(this, "CodePipelinePostToGithub", {
-      pipeline: pipeline.codePipeline,
-      githubToken: githubToken,
-    });
     // testingStage.addPre(
     //   new ShellStep("Run Unit Tests", { commands: ["npm install", "npm test"] })
     // );
